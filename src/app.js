b4557da749fa75b4ed217ed672e691dfc7d5ac80
Aryan Sinha <aryansinha2172007@gmail.com>
2026-03-05 23:47:35 +0530

    Add basic authentication to hide app from public access

--- src/app.js
+++ src/app.js
@@ -8,6 +8,30 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+        return res.status(401).send('Authentication required');
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+
+    const expectedUsername = process.env.AUTH_USERNAME || 'admin';
+    const expectedPassword = process.env.AUTH_PASSWORD || 'password';
+
+    if (username === expectedUsername && password === expectedPassword) {
+        return next();
+    } else {
+        res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+        return res.status(401).send('Invalid credentials');
+    }
+};
+
+app.use(auth);
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
