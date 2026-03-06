d8c0c69bf6d6b2347f3d002109e2f22b537b2ca2
Aryan <aryan2172007@gmail.com>
2026-03-06 20:31:01 +0530

    Update mongo.js

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,13 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectMongo = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('✅ MongoDB Connected');
-    } catch (error) {
-        console.error('MongoDB Error:', error);
-        throw error;
-    }
+let isConnected = false;
+
+export const connectMongo = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "seatbooking",
+    });
+
+    isConnected = conn.connections[0].readyState === 1;
+
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Error:", error);
+    throw error;
+  }
 };
-
-export { connectMongo };
