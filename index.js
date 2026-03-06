286891d7fd486e9a09bc724288a696c20cc1017c
Aryan Sinha <aryansinha2172007@gmail.com>
2026-03-06 19:37:32 +0530

    remove root index.js to avoid Vercel confusion

--- index.js
+++
@@ -1,7 +0,0 @@
-// this file is only used by the Vercel builder/runtime as the
-// exported entrypoint. all startup logic lives in server.js for
-// local development.
-
-import app from './src/app.js';
-
-export default app;
